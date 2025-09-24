7f6a57003f9c193cabc1e15876ffedcea4b7fdd8
Kacper Wojciechowski <39823706+jog1t@users.noreply.github.com>
2025-09-24 21:13:57 +0200

    refactor(cloud): polish

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,7 +1,11 @@
 import type { Clerk } from "@clerk/clerk-js";
 import * as Sentry from "@sentry/react";
 import type { QueryClient } from "@tanstack/react-query";
-import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import {
+	createRootRouteWithContext,
+	Outlet,
+	redirect,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import posthog from "posthog-js";
 import { match } from "ts-pattern";
@@ -29,7 +33,7 @@ interface RootRouteContext {
 export const Route = createRootRouteWithContext<RootRouteContext>()({
 	component: RootRoute,
 	pendingComponent: FullscreenLoading,
-	beforeLoad: async ({ context }) => {
+	beforeLoad: async ({ context, location }) => {
 		return match(__APP_TYPE__)
 			.with("cloud", async () => {
 				if (!context.clerk) return;
@@ -40,8 +44,37 @@ export const Route = createRootRouteWithContext<RootRouteContext>()({
 
 				// Wait for Clerk
 				await new Promise((resolve, reject) => {
+					const timeout = setTimeout(() => {
+						Sentry.captureMessage(
+							"Can't confirm identity",
+							"warning",
+						);
+						reject(
+							redirect({
+								to: "/",
+								search: {
+									back: location.pathname,
+								},
+							}),
+						);
+					}, 10_000);
 					context.clerk.on("status", (payload) => {
 						if (payload === "ready") {
+							clearTimeout(timeout);
+							if (!context.clerk.user) {
+								if (location.pathname !== "/") {
+									reject(
+										redirect({
+											to: "/",
+											search: {
+												back: location.pathname,
+											},
+										}),
+									);
+								}
+								return;
+							}
+
 							Sentry.setUser({
 								id: context.clerk.user?.id,
 								email: context.clerk.user?.primaryEmailAddress
@@ -61,10 +94,6 @@ export const Route = createRootRouteWithContext<RootRouteContext>()({
 							return resolve(true);
 						}
 					});
-					// Timeout to avoid waiting indefinitely
-					setTimeout(() => {
-						reject(new Error("Can't confirm identity"));
-					}, 10000);
 				});
 			})
 			.otherwise(() => {
